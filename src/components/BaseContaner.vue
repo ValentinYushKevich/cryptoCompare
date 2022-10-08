c24998f163f5b5c155ad2f3b3680ed4ca7db0f3c
Valentin Yushkevich <v.yushkevich@bsl.dev>
2022-10-08 18:04:10 +0300

    add pagination buttons

--- src/components/BaseContaner.vue
+++ src/components/BaseContaner.vue
@@ -3,6 +3,28 @@
     <TikerComponent />
     <SearchInput />
     <CoinsList />
+    <hr class="w-full border-t border-gray-200 mt-4 mb-3" />
+
+    <div class="flex justify-between">
+      <p>Показано 2 результатов из 8</p>
+
+      <div>
+        <button
+          type="button"
+          class="px-4 py-2 mr-3 bg-white rounded-md border-2 border-solid"
+          @click="painationChange('prev')"
+        >
+          Назад
+        </button>
+        <button
+          type="button"
+          class="px-4 py-2 bg-white rounded-md border-2 border-solid"
+          @click="painationChange('next')"
+        >
+          Вперед
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,7 +32,14 @@
 import TikerComponent from "./TikerComponent.vue";
 import SearchInput from "./SearchInput.vue";
 import CoinsList from "./CoinsList.vue";
-export default { components: { TikerComponent, SearchInput, CoinsList } };
+export default {
+  components: { TikerComponent, SearchInput, CoinsList },
+  methods: {
+    painationChange(value) {
+      console.log(value);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
